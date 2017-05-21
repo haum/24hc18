d6d5b9404fb97db8c45780e7387b373829d8838b
JackDesBwa <jackdesbwa@gmail.com>
2017-05-21 11:50:30 +0200

    engine: add initial main

---
+++ engine/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+namespace {
+void usage(const char *appname) {
+	std::cout << "Usage: " << appname
+			  << " <scenario> <ia command 1> [ia command 2]..." << std::endl;
+}
+}
+
+int main(int argc, char *argv[]) {
+	// Configuration reading
+	if (argc <= 0) {
+		usage("marabunta_engine");
+		return 1;
+	}
+	if (argc < 3) {
+		usage(argv[0]);
+		return 1;
+	}
+	std::cout << "Scenario: " << argv[1] << '\n';
+	for (int i = 1; i < argc - 1; ++i)
+		std::cout << "IA" << i << ": " << argv[i + 1] << '\n';
+	std::cout << std::endl;
+
+	return 0;
+}
