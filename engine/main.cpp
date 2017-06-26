903e1b7ff0c6ad823c9f226bc38da49883f4272d
JackDesBwa <jackdesbwa@gmail.com>
2017-06-26 23:41:20 +0200

    engine: add options mechanism

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,33 +1,64 @@
 #include "ant.h"
 #include "rand.h"
 #include <array>
+#include <getopt.h>
 #include <iostream>
 #include <sstream>
 
-namespace {
-void usage(const char *appname) {
-	std::cout << "Usage: " << appname
-			  << " <scenario> <ia command 1> [ia command 2]..." << std::endl;
-}
-}
-
 int main(int argc, char *argv[]) {
-	// Configuration reading
-	if (argc <= 0) {
-		usage("marabunta_engine");
-		return 1;
-	}
-	if (argc < 3) {
+	const char *scenario = "";
+
+	// Check arguments
+	auto usage = [](const char *appname) {
+		std::cerr << "USAGE: " << appname << " -s scenario [OPTION] <ias...>\n"
+				  << "       -s, --scenario=file\tFile of the scenario\n"
+				  << std::endl;
+	};
+	if (argc > 1) {
+		struct option long_options[] = {{"scenario", required_argument, 0, 's'},
+										{0, 0, 0, 0}};
+		int c, option_index;
+		while ((c = getopt_long(argc, argv, "s:", long_options,
+								&option_index)) != -1) {
+			switch (c) {
+			case 's':
+				scenario = optarg;
+				break;
+			default:
+				std::cerr << "ERROR: Unknown option\n";
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+	} else if (argc == 1) {
 		usage(argv[0]);
-		return 1;
+		exit(EXIT_FAILURE);
+	} else {
+		usage("marabunta_engine");
+		exit(EXIT_FAILURE);
 	}
-	std::cout << "Scenario: " << argv[1] << '\n';
-	for (int i = 1; i < argc - 1; ++i)
-		std::cout << "IA" << i << ": " << argv[i + 1] << '\n';
+	if (!strcmp(scenario, "")) {
+		std::cerr << "ERROR: Missing scenario\n";
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	if (argc - optind < 1) {
+		std::cerr << "ERROR: No IA\n";
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	// Display info
+	std::cout << "Scenario: " << scenario << '\n';
+	char *ias[argc - optind];
+	for (int i = 0, ind = optind; ind < argc; ++i, ++ind) {
+		ias[i] = argv[ind];
+		std::cout << "IA" << (i + 1) << ": " << argv[ind] << '\n';
+	}
 	std::cout << std::endl;
 
 	// Test two ants
-	Team team{argv[2]};
+	Team team{ias[0]};
 	std::array<Ant, 2> ants = {{Ant{team, 0, 0, 0}, Ant{team, 0, 0, 0}}};
 	for (int i = 0; i < 2000; ++i) {
 		for (auto &ant : ants) {
