fc6c9ee9fc54339b6f57801b684eacc5cee0dbf8
JackDesBwa <jackdesbwa@gmail.com>
2017-12-16 13:23:09 +0100

    engine: add available quantity in Food object

--- engine/food.h
+++ engine/food.h
@@ -12,12 +12,18 @@ class Food : public GameObject {
 	 */
 	Food(double latitude, double longitude);
 
+	/** Get food quantity **/
+	int available() const { return m_available; }
+
 	/** Get category **/
 	static GameObject_t category() { return &s_category; }
 
   private:
 	/** Food category unique object **/
 	static const GameObjectCategory s_category;
+
+	/** available food **/
+	int m_available{0};
 };
 
 #endif // FOOD_H
