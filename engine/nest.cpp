9f7d94ad4ea0c9cc8e9a34d35ddbe20f6b4d6c43
neolinki <neolinki@neodarz.net>
2017-12-21 21:59:44 +0100

    add nest functions(new and out) and variables for it

--- engine/nest.cpp
+++ engine/nest.cpp
@@ -1,20 +1,73 @@
 #include "nest.h"
+#include "ant.h"
+#include "gears/rand.h"
+#include "scenario.h"
 
 const GameObjectCategory Nest::s_category{"NEST"};
 
 Nest::Nest(Team &team, double latitude, double longitude,
            uint64_t initialAntNumber)
-    : Agent{team, Nest::category(), latitude, longitude, 0},
-      m_antNumber{initialAntNumber} {
+    : Agent{team, Nest::category(), latitude, longitude, 0} {
+	addNewAntType(0);
+	antQuantityChanger(0, initialAntNumber);
 	m_teamBase = &team;
 }
 
 bool Nest::prelude(std::ostream &os) {
 	os << "BEGIN NEST\n";
-	os << "ANTS " << static_cast<int>(m_antNumber) << '\n';
+	os << "ANTS " << static_cast<int>(getAntNumberOfType(0)) << '\n';
 	os << "END\n";
 	os.flush();
 	return true;
 }
 
-void Nest::execute(uint8_t /*argc*/, const char ** /*argv*/) {}
+void Nest::invalidAction() {
+	log("Invalid action, Energy divided by 2");
+	m_life /= 2;
+}
+
+void Nest::periodic() { this->setLife(m_life - 1); }
+
+void Nest::actionAntOut(bool valid, uint8_t type, uint8_t m0, uint8_t m1) {
+	if (!valid) {
+		invalidAction();
+		return;
+	}
+	team().scenario().addGameObject<Ant>(team(), 200, this->longitude(),
+	                                     this->latitude(), random_angle(), type,
+	                                     m0, m1);
+	antQuantityChanger(type, -1);
+}
+
+void Nest::actionAntNew(bool valid, uint8_t type) {
+	if (!valid) {
+		invalidAction();
+		return;
+	}
+	if (checkAntType(type)) {
+		antQuantityChanger(type, 1);
+	} else {
+		addNewAntType(type);
+	}
+}
+
+void Nest::execute(uint8_t argc, const char **argv) {
+	if (argc <= 0)
+		return;
+	if (!strncmp(argv[0], "ANT_OUT", 8) && argc == 4) {
+		bool ok_type;
+		int type = param_int(argv[1], ok_type, 0, 255);
+		bool ok_m0;
+		int m0 = param_int(argv[2], ok_m0, 0, 255);
+		bool ok_m1;
+		int m1 = param_int(argv[3], ok_m1, 0, 255);
+		bool ok = ok_type && ok_m0 && ok_m1;
+		actionAntOut(ok, type, m0, m1);
+	} else if (!strncmp(argv[0], "ANT_NEW", 8) && argc == 2) {
+		bool ok;
+		int type = param_int(argv[1], ok, 0, 255);
+		actionAntNew(ok, type);
+	} else {
+		invalidAction();
+	}
+}
--- engine/nest.h
+++ engine/nest.h
@@ -3,6 +3,7 @@
 
 #include "gears/agent.h"
 #include "team.h"
+#include <map>
 
 /** Class representing a nest **/
 class Nest : public Agent {
@@ -19,9 +20,55 @@ class Nest : public Agent {
 	/** Get category **/
 	static GameObject_t category() { return &s_category; }
 
+	/** Actions when command are wrong **/
+	void invalidAction();
+
+	/** Get nest life **/
+	int life() { return m_life; }
+
+	/** Set nest life **/
+	void setLife(int life) { this->m_life = life; }
+
+	/** Method called periodically by the engine **/
+	void periodic() override;
+
+	/** Create a new ant in the nest **/
+	void actionAntNew(bool valid, uint8_t type);
+
+	/** An ant already created get out the nest **/
+	void actionAntOut(bool valid, uint8_t type, uint8_t m0, uint8_t m1);
+
+	/** Check if ant of this type already exist **/
+	bool checkAntType(uint8_t type) {
+		if (m_antNumber.count(type) > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/** Add or Remove ants **/
+	void antQuantityChanger(uint8_t type, int changeValue) {
+		m_antNumber[type] = m_antNumber[type] + changeValue;
+	}
+
+	/** Set new type of ant **/
+	void addNewAntType(uint8_t type) { m_antNumber[type] = 1; }
+
+	/** Return how many ant of a type there are **/
+	int getAntNumberOfType(uint8_t type) {
+		if (!checkAntType(type)) {
+			addNewAntType(type);
+			antQuantityChanger(type, -1);
+		}
+		return m_antNumber[type];
+	}
+
   private:
+	/** Life of the nest**/
+	int m_life;
+
 	/** Number of ants currently in nest **/
-	uint64_t m_antNumber;
+	std::map<uint8_t, uint32_t> m_antNumber;
 
 	virtual bool prelude(std::ostream &os) override;
 	virtual void execute(uint8_t argc, const char **argv) override;
