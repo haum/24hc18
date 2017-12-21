53e93f6e39c365784610ae074fa8383b798f74ae
JackDesBwa <jackdesbwa@gmail.com>
2017-12-21 20:10:52 +0100

    engine: add skeleton in scenario

--- engine/scenario.cpp
+++ engine/scenario.cpp
@@ -1,38 +1,205 @@
 #include "scenario.h"
 #include "gears/timeutils.h"
+#include "gears/utils.h"
+#include <climits>
+#include <cmath>
 
 #include "ant.h"
+#include "food.h"
 #include "nest.h"
+#include "pheromone.h"
+
+namespace {
+constexpr int MIN_TEAMS = 1;
+constexpr int MAX_TEAMS = 20;
+constexpr int MIN_U8 = 0;
+constexpr int MAX_U8 = 255;
+constexpr int MIN_LATITUDE = -90;
+constexpr int MAX_LATITUDE = 90;
+constexpr int MIN_LONGITUDE = -180;
+constexpr int MAX_LONGITUDE = 180;
+constexpr int MIN_HEADING = -180;
+constexpr int MAX_HEADING = 180;
+} // namespace
+
+void Scenario::actionMaxteams(uint8_t teams) {
+	if (static_cast<unsigned int>(teams) < m_teams.size())
+		throw(std::runtime_error("Too many teams to play on this scenario"));
+}
+
+void Scenario::actionDuration(int s) { m_duration = s * 1s; }
+
+void Scenario::actionNestPosition(uint8_t team, int /*latitude*/,
+                                  int /*longitude*/) {
+	auto *nest = findNest(team);
+	if (nest) {
+		// nest->setPosition(latitude * M_PI / 180, longitude * M_PI / 180);
+	}
+}
+
+void Scenario::actionNestPopulation(uint8_t team, uint8_t /*type*/,
+                                    int /*nb*/) {
+	auto *nest = findNest(team);
+	if (nest) {
+		// nest->setPopulation(type, nb);
+	}
+}
+
+void Scenario::actionNestFood(uint8_t team, int /*amount*/) {
+	auto *nest = findNest(team);
+	if (nest) {
+		// nest->setFood(amount);
+	}
+}
+
+void Scenario::actionAnt(uint8_t team, uint8_t type, int latitude,
+                         int longitude, int heading, int stamina,
+                         uint8_t memory1, uint8_t memory2) {
+	auto teamPtr = findTeam(team);
+	if (teamPtr != nullptr)
+		addGameObject<Ant>(*teamPtr, stamina, latitude * M_PI / 180,
+		                   longitude * M_PI / 180, heading * M_PI / 180, type,
+		                   memory1, memory2);
+}
+
+void Scenario::actionPheromone(uint8_t team, uint8_t type, int latitude,
+                               int longitude) {
+	auto teamPtr = findTeam(team);
+	if (teamPtr != nullptr)
+		addGameObject<Pheromone>(latitude * M_PI / 180, longitude * M_PI / 180,
+		                         *teamPtr, type);
+}
+
+void Scenario::actionFood(int latitude, int longitude) {
+	addGameObject<Food>(latitude * M_PI / 180, longitude * M_PI / 180);
+}
+
+Team *Scenario::findTeam(uint8_t team) {
+	if (team >= m_teams.size())
+		return nullptr;
+	return m_teams[static_cast<size_t>(team)];
+}
+
+Nest *Scenario::findNest(uint8_t team) {
+	Nest *ret = nullptr;
+	auto teamPtr = findTeam(team);
+	if (teamPtr == nullptr)
+		return ret;
+	listObjects([&ret, teamPtr](auto sgo) {
+		if (sgo->category() == Nest::category()) {
+			auto *nest = static_cast<Nest *>(sgo.get());
+			if (&nest->team() == teamPtr) {
+				ret = nest;
+				return false;
+			}
+		}
+		return true;
+	});
+	if (ret == nullptr) {
+		addGameObject<Nest>(*teamPtr, 0, 0, 0);
+	}
+	return ret;
+}
 
 void Scenario::processLine(uint8_t argc, const char **argv) {
 	if (argc <= 0)
 		return;
 
-	if (!strncmp(argv[0], "NEST", 4) && argc == 5) {
-		int team = atoi(argv[1]);
-		if (team < 0 || static_cast<unsigned int>(team) >= m_teams.size())
-			return;
-		addGameObject<Nest>(*m_teams[team], atof(argv[2]), atof(argv[3]),
-		                    atoi(argv[4]));
+	bool ok;
+	if (!strncmp(argv[0], "ANT", 4) && argc == 9) {
+		auto team = parse_int(argv[1], ok, MIN_TEAMS, MAX_TEAMS);
+		if (!ok)
+			throw(std::runtime_error("Invalid team number"));
+		auto type = parse_int(argv[2], ok, MIN_U8, MAX_U8);
+		if (!ok)
+			throw(std::runtime_error("Invalid type number"));
+		auto latitude = parse_int(argv[3], ok, MIN_LATITUDE, MAX_LATITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid latitude"));
+		auto longitude = parse_int(argv[4], ok, MIN_LONGITUDE, MAX_LONGITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid longitude"));
+		auto heading = parse_int(argv[5], ok, MIN_HEADING, MAX_HEADING);
+		if (!ok)
+			throw(std::runtime_error("Invalid heading"));
+		auto stamina =
+		    parse_int(argv[6], ok, 0, std::numeric_limits<int>::max());
+		auto memory1 = parse_int(argv[7], ok, MIN_U8, MAX_U8);
+		auto memory2 = parse_int(argv[8], ok, MIN_U8, MAX_U8);
+		actionAnt(static_cast<uint8_t>(team), static_cast<uint8_t>(type),
+		          latitude, longitude, heading, stamina,
+		          static_cast<uint8_t>(memory1), static_cast<uint8_t>(memory2));
 
-	} else if (!strncmp(argv[0], "ANT", 3) && argc == 5) {
-		int team = atoi(argv[1]);
-		if (team < 0 || static_cast<unsigned int>(team) >= m_teams.size())
-			return;
-		addGameObject<Ant>(*m_teams[team], 100, atof(argv[2]), atof(argv[3]),
-		                   atof(argv[4]));
+	} else if (!strncmp(argv[0], "PHEROMONE", 10) && argc == 5) {
+		auto team = parse_int(argv[1], ok, MIN_TEAMS, MAX_TEAMS);
+		if (!ok)
+			throw(std::runtime_error("Invalid team number"));
+		auto type = parse_int(argv[2], ok, MIN_U8, MAX_U8);
+		if (!ok)
+			throw(std::runtime_error("Invalid type number"));
+		auto latitude = parse_int(argv[3], ok, MIN_LATITUDE, MAX_LATITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid latitude"));
+		auto longitude = parse_int(argv[4], ok, MIN_LONGITUDE, MAX_LONGITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid longitude"));
+		actionPheromone(static_cast<uint8_t>(team), static_cast<uint8_t>(type),
+		                latitude, longitude);
 
-	} else if (!strncmp(argv[0], "MAXTEAMS", 8) && argc == 2) {
-		auto teams = atoi(argv[1]);
-		if (teams < 0)
-			throw(std::runtime_error("Wrong number of teams"));
-		if (static_cast<unsigned int>(teams) < m_teams.size())
-			throw(
-			    std::runtime_error("Too many teams to play on this scenario"));
-	} else if (!strncmp(argv[0], "DURATION", 8) && argc == 2) {
-		auto duration = atoi(argv[1]);
-		if (duration <= 0)
+	} else if (!strncmp(argv[0], "FOOD", 5) && argc == 3) {
+		auto latitude = parse_int(argv[1], ok, MIN_LATITUDE, MAX_LATITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid latitude"));
+		auto longitude = parse_int(argv[2], ok, MIN_LONGITUDE, MAX_LONGITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid longitude"));
+		actionFood(latitude, longitude);
+
+	} else if (!strncmp(argv[0], "NEST_POSITION", 14) && argc == 4) {
+		auto team = parse_int(argv[1], ok, MIN_TEAMS, MAX_TEAMS);
+		if (!ok)
+			throw(std::runtime_error("Invalid team number"));
+		auto latitude = parse_int(argv[2], ok, MIN_LATITUDE, MAX_LATITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid latitude"));
+		auto longitude = parse_int(argv[3], ok, MIN_LONGITUDE, MAX_LONGITUDE);
+		if (!ok)
+			throw(std::runtime_error("Invalid longitude"));
+		actionNestPosition(static_cast<uint8_t>(team), latitude, longitude);
+
+	} else if (!strncmp(argv[0], "NEST_FOOD", 10) && argc == 3) {
+		auto team = parse_int(argv[1], ok, MIN_TEAMS, MAX_TEAMS);
+		if (!ok)
+			throw(std::runtime_error("Invalid team number"));
+		auto amount =
+		    parse_int(argv[2], ok, 0, std::numeric_limits<int>::max());
+		if (!ok)
+			throw(std::runtime_error("Invalid amount"));
+		actionNestFood(static_cast<uint8_t>(team), amount);
+
+	} else if (!strncmp(argv[0], "NEST_POPULATION", 16) && argc == 4) {
+		auto team = parse_int(argv[1], ok, MIN_TEAMS, MAX_TEAMS);
+		if (!ok)
+			throw(std::runtime_error("Invalid team number"));
+		auto type = parse_int(argv[2], ok, MIN_U8, MAX_U8);
+		if (!ok)
+			throw(std::runtime_error("Invalid type number"));
+		auto nb = parse_int(argv[3], ok, 0, std::numeric_limits<int>::max());
+		if (!ok)
+			throw(std::runtime_error("Invalid nb"));
+		actionNestPopulation(static_cast<uint8_t>(team),
+		                     static_cast<uint8_t>(type), nb);
+
+	} else if (!strncmp(argv[0], "MAXTEAMS", 9) && argc == 2) {
+		auto teams = parse_int(argv[1], ok, MIN_TEAMS, MAX_TEAMS);
+		if (!ok)
+			throw(std::runtime_error("Invalid teams number"));
+		actionMaxteams(static_cast<uint8_t>(teams));
+
+	} else if (!strncmp(argv[0], "DURATION", 9) && argc == 2) {
+		auto duration = parse_int(argv[1], ok, 1, INT_MAX);
+		if (!ok)
 			throw(std::runtime_error("Invalid duration"));
-		m_duration = duration * 1s;
+		actionDuration(duration);
 	}
 }
--- engine/scenario.h
+++ engine/scenario.h
@@ -12,6 +12,45 @@ class Scenario : public ScenarioBase {
 
 	/** Process one line of scenario **/
 	void processLine(uint8_t argc, const char **argv);
+
+	/** action MAXTEAMS **/
+	void actionMaxteams(uint8_t teams);
+
+	/** action DURATION **/
+	void actionDuration(int s);
+
+	/** action NEST_POSITION **/
+	void actionNestPosition(uint8_t team, int latitude, int longitude);
+
+	/** action NEST_POPULATION **/
+	void actionNestPopulation(uint8_t team, uint8_t type, int nb);
+
+	/** action NEST_FOOD **/
+	void actionNestFood(uint8_t team, int amount);
+
+	/** action ANT **/
+	void actionAnt(uint8_t team, uint8_t type, int latitude, int longitude,
+	               int heading, int stamina, uint8_t memory1, uint8_t memory2);
+
+	/** action PHEROMONE **/
+	void actionPheromone(uint8_t team, uint8_t type, int latitude,
+	                     int longitude);
+
+	/** action FOOD **/
+	void actionFood(int latitude, int longitude);
+
+  private:
+	/** Find team
+	 * @param team Team to search
+	 * @return Found team pointer or nullptr
+	 */
+	Team *findTeam(uint8_t team);
+
+	/** Find nest
+	 * @param team Team to search
+	 * @return Found nest pointer or nullptr
+	 */
+	Nest *findNest(uint8_t team);
 };
 
 #endif // SCENARIO_H
