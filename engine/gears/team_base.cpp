8383b83e1e89d04e51d6191eb287326e95cd4bd2
JackDesBwa <jackdesbwa@gmail.com>
2018-01-02 08:16:17 +0100

    engine: disable process dispatch because of bug

--- engine/gears/team_base.cpp
+++ engine/gears/team_base.cpp
@@ -112,6 +112,7 @@ void TeamBase::sendPrelude() {
 			    static_cast<int>(m_teamManagers.size() - 1),
 			    static_cast<int>(random_unit() * (m_teamManagers.size())));
 			m_currentManager = m_teamManagers.begin();
+			managerIndex = 0; // Disable randomness because of bug
 			std::advance(m_currentManager, managerIndex);
 			const auto data = os.str();
 			send(data.c_str(), data.length());
