fc1b7c827d0a6812e0f7c5cd032e4377c18d1101
JbLb <jerome@jblb.net>
2018-01-04 23:54:10 +0100

    engine: another python exemple

---
+++ strategies/other_python/strategies.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+"""
+# scenario.cfg
+
+MAXTEAMS 1
+DURATION 1
+NEST_POSITION 0 20 0
+NEST_POPULATION 0 10 5
+NEST_POPULATION 0 15 5
+NEST_FOOD 0 20
+FOOD 25 5 5 1 200 1000000 0
+
+"""
+
+import sys
+
+def do_nothing():
+    return
+
+isAnt = False
+isNest =False
+
+for line in sys.stdin:
+    #line.upper()
+    line = line.strip()
+
+    if isNest: # on recupere les infos sur le nid
+        if line:
+            line = line.split()
+            if line[0] == "STOCK":
+                do_nothing()
+            elif line[0] == "MEMORY":
+                do_nothing()
+            elif line[0] == "ANT_COUNT": # infos fourmis
+                if ((line[1] == "10") & (int(line[2]) > 0)): #si type 10 et qu'il y'en a encore
+                    repNest ="ANT_OUT 10 0 0" # elle sort du nid
+            elif line[0] == "END":
+                isNest =False
+                print (repNest)
+                print("END", flush=True)
+    elif isAnt: # on recupere les infos sur la fourmi
+        if line:
+            line = line.split()
+            if line[0] == "TYPE":
+                do_nothing()
+            elif line[0] == "MEMORY":
+                if int(line[1]) < 10: # la fourmis explore 10 fois
+                    steep_move = int(line[1])+1
+                    repAnt = repAnt + f"SET_MEMORY {str(steep_move)} {line[2]} \n" # on stock le nombre de move
+                    repAnt = repAnt + f"EXPLORE"
+                else:
+                    repAnt = repAnt + f"SUICIDE" # si deja 1 explorations SUICIDE
+            elif line[0] == "END":
+                isAnt = False
+                print (repAnt)
+                print("END", flush=True)
+
+
+    if line == "BEGIN ANT":
+        isAnt = True
+        repAnt = ""
+    elif line == "BEGIN NEST":
+        isNest = True
+        repNest = ""
+
