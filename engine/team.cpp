10175eb61252a22e26a3525125f50845d149dd5a
JackDesBwa <jackdesbwa@gmail.com>
2018-01-07 23:27:03 +0100

    engine: add more stats

--- engine/team.cpp
+++ engine/team.cpp
@@ -1,9 +1,31 @@
 #include "team.h"
+#include "nest.h"
+#include "scenario.h"
 #include <iostream>
 
 void Team::printStats() {
-	std::cout << "Team " << m_exe << '\n';
-	std::cout << m_agents.size() << " agents alive" << '\n';
-	std::cout << m_stats_agents << " strategies executed" << '\n';
+	Nest *nest = nullptr;
+	scenario().listObjects([&nest, this](auto sgo) {
+		if (sgo->category() == Nest::category()) {
+			auto *n = static_cast<Nest *>(sgo.get());
+			if (&n->team() == this) {
+				nest = n;
+				return false;
+			}
+		}
+		return true;
+	});
+
+	std::cout << "Team executable: " << m_exe << '\n';
+	std::cout << "Executed strategies: " << m_stats_agents << '\n';
+	std::cout << "Alive agents: " << m_agents.size() << '\n';
+	if (nest) {
+		size_t score = nest->foodAmount() + nest->antsCount();
+		score += m_agents.size() - 1;
+
+		std::cout << "Food amount in nest: " << nest->foodAmount() << '\n';
+		std::cout << "Ants inside nest: " << nest->antsCount() << '\n';
+		std::cout << "Score: " << score << '\n';
+	}
 	std::cout << std::flush;
 }
