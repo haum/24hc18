0a29ff14b177126a5ff6cebdd96a7c80607fc5be
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2018-01-09 23:49:18 +0100

    Fix launch script

--- scores/scripts/launch.sh
+++ scores/scripts/launch.sh
@@ -4,8 +4,9 @@ source set_env.sh
 
 run_file_path="$MBNT_URL/be/t:$MBNT_TOKEN/run_file"
 run_file=$(curl -s $run_file_path)
-for l in $run_file;
-do
+
+IFS=$'\n'
+for l in $run_file; do
  [[ $l == '' ]] && continue
  echo line : $l
  # do GNU PARALLEL MAGIC
