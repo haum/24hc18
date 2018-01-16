ebebe86d874093b97097641959122306a8bcf517
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2018-01-17 00:09:57 +0100

    strategies:matael: unproofread update

--- strategies/python_stack/strats.py
+++ strategies/python_stack/strats.py
@@ -39,7 +39,7 @@ def nest_strat(n):
 
     if n.ants_in != []:
         for a in n.ants_in:
-            if a['type'] == 0 and a['memory'][0] > 0:
+            if a['type'] == 0 and a['memory'][1] > 0:
                 n.memory[0] = 1
 
     if n.ants != dict():
@@ -54,8 +54,7 @@ def nest_strat(n):
         output.append('ANT_NEW 1')
 
     memstring = ' '.join(map(str, n.memory))
-    output.append(f'SET_MEMORY {memstring}')
-    return output
+    return [f'SET_MEMORY {memstring}'] + output
 
 
 def ant_strat(a):
@@ -83,38 +82,40 @@ def ant_strat(a):
         ant_id = a.memory[0] - 100*floor(a.memory[0]/100)
         found_food_last_time = a.memory[1] >= 100
         found_food = (a.memory[1] - 100*floor(a.memory[1]/100)) == 1
-
-        self_pheromones = [_['id'] for _ in a.pheromones if _['type'] == ant_id]
-        try:
-            target_pheromone = sorted(self_pheromones, key=lambda _: _.get('dist'))[-1]
-        except:
-            target_pheromone = False
+        current_pheromone = False
+        target_pheromone = False
 
         try:
-            current_pheromone = sorted(self_pheromones, key=lambda _: _.get('dist'))[0]
-        except:
-            current_pheromone = False
+            self_pheromones = filter(lambda _: _['type'] == ant_id, a.pheromones)
+            self_pheromones = [_['id'] for _ in sorted(self_pheromones, key=lambda _: _.get('dist'))]
+            target_pheromone = self_pheromones[-1]
+            current_pheromone = self_pheromones[0]
+        except IndexError:
+            pass
 
-        if a.memory[1] > 0:
+        if found_food_last_time:
+            output.append('TURN 180')
+            excl = True
+            a.memory[1] -= 100
             if moved_last:
-                if found_food_last_time:
-                    output.append('TURN 180')
-                    excl = True
-                    a.memory[1] -= 100
-                elif found_food:
-                    output.append(f'CHANGE_PHEROMOME {current_pheromone} 10')
-                    excl = True
-                    a.memory[1] = 1
+                a.memory[0] -= 100
+        elif found_food:
+            if moved_last and current_pheromone:
+                output.append(f'CHANGE_PHEROMONE {current_pheromone} 10')
+                excl = True
                 a.memory[0] -= floor(a.memory[0]/100)*100
             elif not moved_last and (target_pheromone or nest):
-                if nest:
+                if nest_near:
+                    log('nest')
+                    output.append(f'NEST {nest_near}')
+                elif nest:
                     output.append(f'MOVE_TO {nest}')
                 else:
                     output.append(f'MOVE_TO {target_pheromone}')
                 a.memory[0] += 100
             else:
-                log('SUICIDE !')
-                output.append('SUICIDE')
+                pass
+                # output.append('SUICIDE')
         else:
             if moved_last:
                 if a.food != []:
@@ -131,9 +132,40 @@ def ant_strat(a):
 
     elif a.typ == 1:  # gatherer
         has_turned = a.memory[0] == 1
-        pheromones = [_['id'] for _ in a.pheromones if _['typ'] == 10]
+        pheromones = [_['id'] for _ in a.pheromones if _['type'] == 10]
         try:
             target_pheromone = sorted(pheromones, key=lambda _: _.get('dist'))[-1]
+        except:
+            target_pheromone = False
+        food_near = [_['id'] for _ in a.food if _['zone'] == 'NEAR']
+
+        if a.stock == 0:  # not gathered yet
+            if a.food != []:
+                if food_near != []:
+                    output.append(f'COLLECT {food_near[0]} 100')
+                else:
+                    food_id = sorted(a.food, key=lambda _: _['dist'])[0]
+                    output.append(f'MOVE_TO f{food_id}')
+            if target_pheromone:
+                output.append(f'MOVE_TO f{target_pheromone}')
+        else:  # gathered
+            if food_near != [] and not has_turned:
+                output.append(f'TURN 180')
+                a.memory[0] = 1
+            elif nest_near:
+                output.append(f'NEST {nest_near}')
+            elif nest:
+                output.append(f'MOVE_TO {nest}')
+            else:
+                if target_pheromone:
+                    output.append(f'MOVE_TO f{target_pheromone}')
+
+    elif a.typ == 2:  # gatherer
+        has_turned = a.memory[0] == 1
+        pheromones = filter(lambda _: _['type'] == 10, a.pheromones)
+        pheromones = [_['id'] for _ in sorted(self_pheromones, key=lambda _: _.get('dist'))]
+        try:
+            target_pheromone = pheromones[-1]
         except:
             target_pheromone = False
         food_near = [_['id'] for _ in a.food if _['zone'] == 'NEAR']
@@ -159,5 +191,4 @@ def ant_strat(a):
                     output.append(f'MOVE_TO f{target_pheromone}')
 
     memstring = ' '.join(map(str, a.memory))
-    output.append(f'SET_MEMORY {memstring}')
-    return output
+    return [f'SET_MEMORY {memstring}'] + output
